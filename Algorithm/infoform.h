f1d25a0912507af10d65f93e02b5fbddafccc348
ImortalMashine <debughome@yandex.ru>
2018-02-12 14:03:55 +0300

    redesign infoform

--- Algorithm/infoform.h
+++ Algorithm/infoform.h
@@ -58,6 +58,8 @@ namespace Algorithm {
 
 	private: System::Windows::Forms::Label^  tagCOLLEGE;
 	private: System::Windows::Forms::Label^  COLLEGE;
+	private: System::Windows::Forms::PictureBox^  aboutPIC;
+
 
 
 
@@ -78,6 +80,7 @@ namespace Algorithm {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(infoform::typeid));
 			this->tagOWNER = (gcnew System::Windows::Forms::Label());
 			this->tagSTAGE = (gcnew System::Windows::Forms::Label());
 			this->tagGROUP = (gcnew System::Windows::Forms::Label());
@@ -90,127 +93,159 @@ namespace Algorithm {
 			this->GITHUB = (gcnew System::Windows::Forms::LinkLabel());
 			this->tagCOLLEGE = (gcnew System::Windows::Forms::Label());
 			this->COLLEGE = (gcnew System::Windows::Forms::Label());
+			this->aboutPIC = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->aboutPIC))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// tagOWNER
 			// 
 			this->tagOWNER->AutoSize = true;
-			this->tagOWNER->Location = System::Drawing::Point(12, 30);
+			this->tagOWNER->Font = (gcnew System::Drawing::Font(L"Perpetua", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->tagOWNER->Location = System::Drawing::Point(7, 22);
 			this->tagOWNER->Name = L"tagOWNER";
-			this->tagOWNER->Size = System::Drawing::Size(83, 20);
+			this->tagOWNER->Size = System::Drawing::Size(95, 22);
 			this->tagOWNER->TabIndex = 0;
 			this->tagOWNER->Text = L"Developer :";
 			// 
 			// tagSTAGE
 			// 
 			this->tagSTAGE->AutoSize = true;
-			this->tagSTAGE->Location = System::Drawing::Point(40, 98);
+			this->tagSTAGE->Font = (gcnew System::Drawing::Font(L"Perpetua", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->tagSTAGE->Location = System::Drawing::Point(35, 89);
 			this->tagSTAGE->Name = L"tagSTAGE";
-			this->tagSTAGE->Size = System::Drawing::Size(50, 20);
+			this->tagSTAGE->Size = System::Drawing::Size(57, 22);
 			this->tagSTAGE->TabIndex = 1;
 			this->tagSTAGE->Text = L"Stage :";
 			// 
 			// tagGROUP
 			// 
 			this->tagGROUP->AutoSize = true;
-			this->tagGROUP->Location = System::Drawing::Point(37, 132);
+			this->tagGROUP->Font = (gcnew System::Drawing::Font(L"Perpetua", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->tagGROUP->Location = System::Drawing::Point(32, 124);
 			this->tagGROUP->Name = L"tagGROUP";
-			this->tagGROUP->Size = System::Drawing::Size(59, 20);
+			this->tagGROUP->Size = System::Drawing::Size(66, 22);
 			this->tagGROUP->TabIndex = 2;
 			this->tagGROUP->Text = L"Group :";
 			// 
 			// tagTASK
 			// 
 			this->tagTASK->AutoSize = true;
-			this->tagTASK->Location = System::Drawing::Point(47, 165);
+			this->tagTASK->Font = (gcnew System::Drawing::Font(L"Perpetua", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->tagTASK->Location = System::Drawing::Point(40, 207);
 			this->tagTASK->Name = L"tagTASK";
-			this->tagTASK->Size = System::Drawing::Size(46, 20);
+			this->tagTASK->Size = System::Drawing::Size(52, 22);
 			this->tagTASK->TabIndex = 3;
 			this->tagTASK->Text = L"Task :";
 			// 
 			// OWNER
 			// 
 			this->OWNER->AutoSize = true;
-			this->OWNER->Location = System::Drawing::Point(99, 30);
+			this->OWNER->Font = (gcnew System::Drawing::Font(L"Perpetua", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->OWNER->Location = System::Drawing::Point(94, 22);
 			this->OWNER->Name = L"OWNER";
-			this->OWNER->Size = System::Drawing::Size(103, 20);
+			this->OWNER->Size = System::Drawing::Size(114, 22);
 			this->OWNER->TabIndex = 4;
 			this->OWNER->Text = L"Фадеев И. С.";
 			// 
 			// STAGE
 			// 
 			this->STAGE->AutoSize = true;
-			this->STAGE->Location = System::Drawing::Point(99, 98);
+			this->STAGE->Font = (gcnew System::Drawing::Font(L"Perpetua", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->STAGE->Location = System::Drawing::Point(94, 89);
 			this->STAGE->Name = L"STAGE";
-			this->STAGE->Size = System::Drawing::Size(17, 20);
+			this->STAGE->Size = System::Drawing::Size(19, 22);
 			this->STAGE->TabIndex = 5;
 			this->STAGE->Text = L"2";
 			// 
 			// TASK
 			// 
 			this->TASK->AutoSize = true;
-			this->TASK->Location = System::Drawing::Point(100, 165);
+			this->TASK->Font = (gcnew System::Drawing::Font(L"Perpetua", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->TASK->Location = System::Drawing::Point(93, 207);
 			this->TASK->Name = L"TASK";
-			this->TASK->Size = System::Drawing::Size(633, 20);
+			this->TASK->Size = System::Drawing::Size(353, 22);
 			this->TASK->TabIndex = 6;
-			this->TASK->Text = L"Реализовать Очередь (АТД) + Сортировка (Шелл) + время инициализации и сортировки";
+			this->TASK->Text = L"Очередь (FIFO) + Сортировка (Shell) + timer";
 			// 
 			// GROUP
 			// 
 			this->GROUP->AutoSize = true;
-			this->GROUP->Location = System::Drawing::Point(99, 132);
+			this->GROUP->Font = (gcnew System::Drawing::Font(L"Perpetua", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->GROUP->Location = System::Drawing::Point(94, 124);
 			this->GROUP->Name = L"GROUP";
-			this->GROUP->Size = System::Drawing::Size(91, 20);
+			this->GROUP->Size = System::Drawing::Size(101, 22);
 			this->GROUP->TabIndex = 7;
 			this->GROUP->Text = L"ВПБВ 01-16";
 			// 
 			// tagGITHUB
 			// 
 			this->tagGITHUB->AutoSize = true;
-			this->tagGITHUB->Font = (gcnew System::Drawing::Font(L"Perpetua", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->tagGITHUB->Font = (gcnew System::Drawing::Font(L"Perpetua", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->tagGITHUB->Location = System::Drawing::Point(325, 216);
+			this->tagGITHUB->Location = System::Drawing::Point(35, 159);
 			this->tagGITHUB->Name = L"tagGITHUB";
-			this->tagGITHUB->Size = System::Drawing::Size(65, 20);
+			this->tagGITHUB->Size = System::Drawing::Size(73, 22);
 			this->tagGITHUB->TabIndex = 8;
 			this->tagGITHUB->Text = L"GitHub :";
 			// 
 			// GITHUB
 			// 
 			this->GITHUB->AutoSize = true;
-			this->GITHUB->Font = (gcnew System::Drawing::Font(L"Perpetua", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->GITHUB->Font = (gcnew System::Drawing::Font(L"Perpetua", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->GITHUB->Location = System::Drawing::Point(391, 216);
+			this->GITHUB->Location = System::Drawing::Point(101, 159);
 			this->GITHUB->Name = L"GITHUB";
-			this->GITHUB->Size = System::Drawing::Size(309, 20);
+			this->GITHUB->Size = System::Drawing::Size(82, 22);
 			this->GITHUB->TabIndex = 9;
 			this->GITHUB->TabStop = true;
 			this->GITHUB->Tag = L"https://github.com/ImortalMashine/Algorithm";
-			this->GITHUB->Text = L"https://github.com/ImortalMashine/Algorithm";
+			this->GITHUB->Text = L"Algorithm";
 			// 
 			// tagCOLLEGE
 			// 
 			this->tagCOLLEGE->AutoSize = true;
-			this->tagCOLLEGE->Location = System::Drawing::Point(30, 65);
+			this->tagCOLLEGE->Font = (gcnew System::Drawing::Font(L"Perpetua", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->tagCOLLEGE->Location = System::Drawing::Point(25, 57);
 			this->tagCOLLEGE->Name = L"tagCOLLEGE";
-			this->tagCOLLEGE->Size = System::Drawing::Size(65, 20);
+			this->tagCOLLEGE->Size = System::Drawing::Size(75, 22);
 			this->tagCOLLEGE->TabIndex = 10;
 			this->tagCOLLEGE->Text = L"College :";
 			// 
 			// COLLEGE
 			// 
 			this->COLLEGE->AutoSize = true;
-			this->COLLEGE->Location = System::Drawing::Point(100, 65);
+			this->COLLEGE->Font = (gcnew System::Drawing::Font(L"Perpetua", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->COLLEGE->Location = System::Drawing::Point(95, 57);
 			this->COLLEGE->Name = L"COLLEGE";
-			this->COLLEGE->Size = System::Drawing::Size(43, 20);
+			this->COLLEGE->Size = System::Drawing::Size(49, 22);
 			this->COLLEGE->TabIndex = 11;
 			this->COLLEGE->Text = L"MTU";
+			// 
+			// aboutPIC
+			// 
+			this->aboutPIC->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"aboutPIC.Image")));
+			this->aboutPIC->Location = System::Drawing::Point(214, 21);
+			this->aboutPIC->Name = L"aboutPIC";
+			this->aboutPIC->Size = System::Drawing::Size(250, 160);
+			this->aboutPIC->TabIndex = 12;
+			this->aboutPIC->TabStop = false;
 			// 
 			// infoform
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(741, 250);
+			this->ClientSize = System::Drawing::Size(478, 250);
+			this->Controls->Add(this->aboutPIC);
 			this->Controls->Add(this->COLLEGE);
 			this->Controls->Add(this->tagCOLLEGE);
 			this->Controls->Add(this->GITHUB);
@@ -233,6 +268,7 @@ namespace Algorithm {
 			this->ShowIcon = false;
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Load += gcnew System::EventHandler(this, &infoform::infoform_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->aboutPIC))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
